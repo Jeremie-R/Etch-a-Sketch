16bd9610d7c8577a4a1d296c51dd2f0f82cde58c
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2023-11-23 20:55:36 +0100

    generated pixel in teh board

--- app.js
+++ app.js
@@ -3,6 +3,23 @@
 
 //build the sketch area
 
+createBoard(8);
+
+function createBoard(a) {
+    let Board = document.getElementById("DrawingArea");
+
+    
+    //goal is to create a list of div that will be the pixels
+    for (let id = 0; id < (a*a); id++) {
+
+        Board.innerHTML += "<div id="+id+" class='pixel'></div>";
+        
+    };
+
+
+
+};
+
 //interact with a cell
 
 //change colour
