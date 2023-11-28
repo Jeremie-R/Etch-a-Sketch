ef60b5f8ab19096026f1dbeb7d86297b9a4412b1
farah <140409352+Jeremie-R@users.noreply.github.com>
2023-11-28 20:16:05 +0100

    i can genersate the grid but not handle the cell

--- app.js
+++ app.js
@@ -4,23 +4,47 @@
 //build the sketch area
 
 createBoard(8);
+let colour = "#000000"
 
 function createBoard(a) {
     let Board = document.getElementById("DrawingArea");
 
     
-    //goal is to create a list of div that will be the pixels
-    for (let id = 0; id < (a*a); id++) {
+    //rows
+    for (let idRow = 0; idRow < (a); idRow++) {
 
-        Board.innerHTML += "<div id="+id+" class='pixel'></div>";
-        
+        Board.innerHTML += "<div id=row"+idRow+" class='row'></div>";
+        let Row = document.getElementById("row"+idRow);
+
+        //pixel in the row
+        for (let id = 0; id < (a); id++) {
+            let cellID = "row"+idRow+"cell"+id;
+            Row.innerHTML += "<div id="+cellID+" class='pixel'></div>";
+            cellInteraction(cellID);
+
+        };
+
+    
     };
-
 
 
 };
 
 //interact with a cell
+
+function cellInteraction(cellID) {
+    
+    let cell = document.getElementById(cellID);
+    
+    cell.addEventListener("click", click(cell, cellID));
+    
+}
+
+function click(cell, cellID) {
+    console.log(cellID);
+    cell.style.backgroundColor = "red";
+    cell.style.opacity = "0.5";
+}
 
 //change colour
 
